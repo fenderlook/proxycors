42f59a0423ed88757164ea021ecdfdcd1aca9a48
Luis Ricardo Ruiz <luislicardo1307@gmail.com>
2020-06-16 15:22:15 -0500

    Allow connect to HTTPS servers. Only for testing purpouse

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
@@ -7,6 +9,13 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
-Object.keys(config.urls || {}).forEach((key) => app.use(`/${key}`, proxy(config.urls[key])));
+Object.keys(config.urls || {}).forEach((key) =>
+  app.use(
+    `/${key}`,
+    proxy(config.urls[key], {
+      https: config.urls[key].startsWith('https://'),
+    })
+  )
+);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
